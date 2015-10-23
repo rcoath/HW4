660170e07ead4b7b5b1ad5e4f8732ec03d39aaaf
Ryan Coath <ryan-coath@uiowa.edu>
2015-10-23 13:16:51 -0500

    toubleshooting errors and edited readQuery

--- src/java/dbHelpers/ReadQuery.java
+++ src/java/dbHelpers/ReadQuery.java
@@ -1,29 +1,27 @@
-
 package dbHelpers;
 
 import java.io.IOException;
 import java.io.InputStream;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.Properties;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import model.Movies;
+
 
 public class ReadQuery {
     
     private Connection conn;
     private ResultSet results;
     
-    
-    
-    public ReadQuery() {
-    
-    
-    Properties props = new Properties();
-    InputStream instr;
-        instr = getClass().getResourcesAsStream("dbConn.properties");
+    public ReadQuery () {
+   
+       Properties props = new Properties(); //MWC
+       InputStream instr = getClass().getResourceAsStream("dbConn.properties");
         try {
             props.load(instr);
         } catch (IOException ex) {
@@ -34,11 +32,11 @@ public class ReadQuery {
         } catch (IOException ex) {
             Logger.getLogger(ReadQuery.class.getName()).log(Level.SEVERE, null, ex);
         }
-    
-    String driver = props.getProperty("driver.name");
-    String url = props.getProperty("server.name");
-    String username = props.getProperty("user.name");
-    String passwd = props.getProperty("user.password");
+       
+       String driver = props.getProperty("driver.name");
+       String url = props.getProperty("server.name");
+       String username = props.getProperty("user.name");
+       String passwd = props.getProperty("user.password");
         try {
             Class.forName(driver);
         } catch (ClassNotFoundException ex) {
@@ -49,45 +47,68 @@ public class ReadQuery {
         } catch (SQLException ex) {
             Logger.getLogger(ReadQuery.class.getName()).log(Level.SEVERE, null, ex);
         }
-    
-        
-        public void doRead
-                
-                String query = "Select * from movie"
-                        PreparedStatement ps = conn.prepareStatement(query);
-                        this.results= ps.executeQuery();
-                        
-    Public String get HTMLTable() {
-        
-   String table = "";
-   table += "<table border=1>";
-   
-   while(this.results.next()){
-   
-       
-       Movies movie = new Movie();
-       movie.setMovieID(this.results.getInt("movieID"));
-         movie.setMovieName(this.results.getInt("movieName"));
-          movie.setReleaseDAte(this.results.getInt("releaseDate"));
-          movie.setLead(this.results.getInt("lead"));
-          movie.setDirector(this.results.getInt("director"));
-   
-   table += "<tr>";
-        table += "<td>";
-            table 
-   
    }
    
-           
-           
-           
-           
+   public void doRead(){
        
-   
-   table += "</table>";
-   
-           return table;
-           
+        try {
+            String query = "Select * from Movies";
             
+            PreparedStatement ps = conn.prepareStatement(query);
+            this.results = ps.executeQuery();
+        } catch (SQLException ex) {
+            Logger.getLogger(ReadQuery.class.getName()).log(Level.SEVERE, null, ex);
+        }
+   }
+   
+   
+   public String getHTMLTable(){
+   
+        String table = "";
+        
+        table += "<table border=1>";
+        
+        try {
+            while(this.results.next()){
+                
+                Movies movies = new Movies();
+                movies.setMovieID(this.results.getInt("movieID"));
+                movies.setMovieName(this.results.getString("movieName"));
+                movies.setReleaseDate(this.results.getInt("releaseDate"));
+                movies.setLead(this.results.getString("lead"));
+                movies.setDirector(this.results.getString("director"));
+                
+                
+                table += "<tr>";
+                table += "<td>";
+                table += movies.getMovieID();
+                table += "</td>";
+                
+                table += "<td>";
+                table += movies.getMovieName();
+                table += "</td>";
+                
+                table += "<td>";
+                table += movies.getReleaseDate();
+                table += "</td>";
+                
+                table += "<td>";
+                table += movies.getLead();
+                table += "</td>";
+
+                table += "<td>";
+                table += movies.getDirector();
+                table += "</td>";
+                table += "</tr>";
+                
+                
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(ReadQuery.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        table += "</table>";
+        
+                return table;
     }
 }
---
+++ web/read.jsp
@@ -0,0 +1,20 @@
+<%-- 
+    Document   : read
+    Created on : Oct 23, 2015, 12:59:25 PM
+    Author     : Ryan
+--%>
+
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>JSP Page</title>
+    </head>
+    
+    <% String table = (String) request.getAttribute("table"); %>
+    <body>
+        <h1>Ryan's Movie List</h1>
+        <%= table %>
+    </body>
+</html>
